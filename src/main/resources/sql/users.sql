7321f2717eed8c8c614064b52b5cc8e23130552e
gribnoff <quinvex88@gmail.com>
2020-12-12 22:03:22 +0300

    users table script

---
+++ src/main/resources/sql/users.sql
@@ -0,0 +1,21 @@
+-- DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS "users";
+
+-- CREATE TABLE `users` (
+--     `username`  VARCHAR(255) NOT NULL,
+--     `password`  VARCHAR(255) NOT NULL,
+--     PRIMARY KEY (`username`)
+create table "users" (
+    "username" varchar(255) not null
+        constraint "users_pk"
+            primary key,
+    "password" varchar(255) not null,
+    "balance" float default 0.0 not null
+);
+
+
+
+-- INSERT INTO `users`
+INSERT INTO "users"
+VALUES ('user', 'user', 555.60),
+       ('tomcat', 'tomcat', 10.18);
